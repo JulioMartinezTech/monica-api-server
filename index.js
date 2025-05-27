de5f88bf4adbb67c10b23fabc15a3288e6f2d3ac
julio martinez <info.julioweb@gmail.com>
2025-05-27 12:38:58 +0200

    creating endpoints

--- index.js
+++ index.js
@@ -1,38 +1,112 @@
 const express = require("express");
 const {
-  getContacts,
-  getContactById,
   getGenders,
+  getCountries,
+  createContact,
+  createContactField,
+  createTag,
+  createAddress,
+  createCompany,
+  createOccupation,
+  createDocument,
 } = require("./services/monica");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/contacts", async (req, res) => {
-  try {
-    const contacts = await getContacts();
-    res.json(contacts);
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener contactos" });
-  }
-});
-
-app.get("/contacts/:id", async (req, res) => {
-  try {
-    const contact = await getContactById(req.params.id);
-    res.json(contact);
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener el contacto" });
-  }
-});
+//get request
 
 app.get("/genders", async (req, res) => {
   try {
     const genders = await getGenders();
     res.json(genders);
   } catch (error) {
-    res.status(500).json({ error: "Error al obtener los generos" });
+    res.status(500).json({ error: "Error to get genders" });
+  }
+});
+
+app.get("/countries", async (req, res) => {
+  try {
+    const countries = await getCountries();
+    res.json(countries);
+  } catch (error) {
+    res.status(500).json({ error: "Error to get countries" });
+  }
+});
+
+//post request
+
+app.post("/contact", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createContact(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating contact" });
+  }
+});
+app.post("/contactfields", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createContactField(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating contact's fields" });
+  }
+});
+app.post("/tags", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createTag(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating tag" });
+  }
+});
+app.post("/associateTag", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createAssociateTag(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating associate tag" });
+  }
+});
+app.post("/address", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createAddress(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating address" });
+  }
+});
+app.post("/company", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createCompany(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating company" });
+  }
+});
+app.post("/occupation", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createOccupation(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating the occupation" });
+  }
+});
+app.post("/document", async (req, res) => {
+  try {
+    const body = req.body;
+    const response = await createDocument(body);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Error when creating document" });
   }
 });
 
--- services/monica.js
+++ services/monica.js
@@ -10,20 +10,117 @@ const monicaAPI = axios.create({
 });
 
 module.exports = {
-  getContacts: async () => {
-    const response = await monicaAPI.get("/contacts");
-    return response.data;
-  },
-
-  getContactById: async (id) => {
-    const response = await monicaAPI.get(`/contacts/${id}`);
-    return response.data;
-  },
+  // get request
 
   getGenders: async () => {
     const response = await monicaAPI.get("/genders");
     return response.data;
   },
 
-  // Puedes agregar más métodos como createContact, updateContact, etc.
+  getCountries: async () => {
+    const response = await monicaAPI.get("/countries");
+    return response.data;
+  },
+
+  //post request
+
+  createContact: async (contactData) => {
+    try {
+      const response = await monicaAPI.post("/contacts", contactData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error to create the contact",
+        error.response?.data || error.message
+      );
+      throw error;
+    }
+  },
+
+  createContactField: async (contactFieldData) => {
+    try {
+      const response = await monicaAPI.post("/contactfields", contactFieldData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error to create the contact's field",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createTag: async (tagData) => {
+    try {
+      const response = await monicaAPI.post("/tags", tagData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error to create the tag",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createAssociateTag: async (associateTagData) => {
+    try {
+      const response = await monicaAPI.post(
+        `/contacts/${associateTagData.id}/setTags`,
+        associateTagData
+      );
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error when creating a tag association",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createAddress: async (addressData) => {
+    try {
+      const response = await monicaAPI.post("/addresses", addressData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error when creating the address",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createCompany: async (companyData) => {
+    try {
+      const response = await monicaAPI.post("/companies", companyData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error when creating the company",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createOccupation: async (occupationData) => {
+    try {
+      const response = await monicaAPI.post("/occupations", occupationData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error when creating the occupation",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  createDocument: async (documentData) => {
+    try {
+      const response = await monicaAPI.post("/documents", documentData);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Error when creating the document",
+        error.response?.data || error.message
+      );
+    }
+  },
 };
